898addd5c43eb62383f58922b6d54d5b2e745a83
Morten Hustveit <morten.hustveit@gmail.com>
2023-04-07 14:43:15 -0400

    Add app icons

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path="", static_folder="")
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return send_from_directory("public", "index.html")
+
+@app.route("/<path:path>")
+def serve_files(path):
+    return send_from_directory("public", path)
 
 if __name__ == "__main__":
     app.run(debug=True)
