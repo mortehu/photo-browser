ef825e268b38a655743ae2234f266dd581cf375b
Morten Hustveit <morten.hustveit@gmail.com>
2023-04-07 13:42:15 -0400

    Serve index file from new location

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path="", static_folder="")
 
 @app.route("/")
 def index():
-    return send_from_directory("", "index.html")
+    return send_from_directory("public", "index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
